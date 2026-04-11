f1672fcf66a554d881d5a60e051438a7f5d3fba3
Mikey1-nuel <emmanuelnwoye12@gmail.com>
2026-04-11 12:26:20 +0100

    Exponged nodemailer and implemented resend for sending emails

--- middleware/sendEmail.js
+++ middleware/sendEmail.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function sendEmail({ to, subject, text, html }) {
   try {
     const response = await resend.emails.send({
